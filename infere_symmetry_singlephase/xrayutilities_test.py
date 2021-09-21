5f91607b3094d4b6a7ebc4e38b6063208536a606
Henrik Schopmans <h.schopmans@gmail.com>
2021-09-21 16:02:27 +0200

    Update default fundamental parameters

--- infere_symmetry_singlephase/xrayutilities_test.py
+++ infere_symmetry_singlephase/xrayutilities_test.py
@@ -3,63 +3,68 @@ from xrayutilities.materials.cif import CIFFile
 import numpy as np
 import matplotlib.pyplot as plt
 
-from pymatgen.io.cif import CifParser
-from pymatgen.analysis.diffraction.xrd import XRDCalculator
 
 crystal = xu.materials.Crystal.fromCIF("test.cif")
 
 powder = xu.simpack.Powder(crystal, 1)
 
-powder_model = xu.simpack.PowderModel(powder, I0=100)
+# Apply all settings
+# Further information on the settings: https://nvlpubs.nist.gov/nistpubs/jres/120/jres.120.014.c.py
+powder_model = xu.simpack.PowderModel(
+    powder,
+    I0=100,
+    fpsettings={
+        "classoptions": {
+            "anglemode": "twotheta",
+            "oversampling": 4,
+            "gaussian_smoother_bins_sigma": 1.0,
+            "window_width": 20.0,
+        },
+        "global": {
+            "geometry": "symmetric",
+            "geometry_incidence_angle": None,
+            "diffractometer_radius": 0.3,
+            "equatorial_divergence_deg": 0.5,
+            "dominant_wavelength": 1.5405929888444028e-10,
+        },
+        "emission": {
+            "emiss_wavelengths": (1.207930e-10),
+            "emiss_intensities": (1.0),
+            "emiss_gauss_widths": (3e-14),
+            "emiss_lor_widths": (3e-14),
+            "crystallite_size_lor": 2e-07,
+            "crystallite_size_gauss": 2e-07,
+            "strain_lor": 0,
+            "strain_gauss": 0,
+            "preferred_orientation": (0, 0, 0),
+            "preferred_orientation_factor": 1,
+        },
+        "axial": {
+            "axDiv": "full",
+            "slit_length_source": 0.008001,
+            "slit_length_target": 0.008,
+            "length_sample": 0.01,
+            "angI_deg": 2.5,
+            "angD_deg": 2.5,
+            "n_integral_points": 10,
+        },
+        "absorption": {"absorption_coefficient": 100000.0},
+        "si_psd": {"si_psd_window_bounds": None},
+        "receiver_slit": {"slit_width": 5.5e-05},
+        "tube_tails": {
+            "main_width": 0.0002,
+            "tail_left": -0.001,
+            "tail_right": 0.001,
+            "tail_intens": 0.001,
+        },
+    },
+)
 
-xs = np.arange(5,120,0.01)
+xs = np.arange(5, 120, 0.01)
 diffractogram = powder_model.simulate(xs)
 
 # Default settings:
-print(powder_model.pdiff[0].settings)
-# Further information on the default settings: https://nvlpubs.nist.gov/nistpubs/jres/120/jres.120.014.c.py
+# print(powder_model.pdiff[0].settings)
 
 powder_model.plot(xs)
-plt.show()
-
-exit()
-
-# do the same with pymatgen:
-
-parser = CifParser("test.cif")
-structure = parser.get_structures()[0]
-
-calculator = XRDCalculator()
-pattern = calculator.get_pattern(structure, scaled=True)
-
-def smeared_peaks(xs):
-
-        output = np.zeros(len(xs))
-
-        for x, y in zip(pattern.x, pattern.y):
-
-            sigma = 0.1
-            mean = x
-
-            ys = (
-                1
-                / (sigma * np.sqrt(2 * np.pi))
-                * np.exp(-1 / (2 * sigma ** 2) * (xs - mean) ** 2)
-            )
-
-            delta_x = xs[1] - xs[0]
-            volume = delta_x * np.sum(ys)
-
-            ys = y * ys / volume
-
-            output += ys
-
-        return output
-
-ys = smeared_peaks(xs)
-
-plt.figure()
-plt.plot(xs, ys)
-plt.title("pymatgen")
-
 plt.show()
