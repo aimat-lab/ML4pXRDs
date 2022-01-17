88ae3523d1a24d20c72b47e2faf8314750f447da
Henrik Schopmans <h.schopmans@gmail.com>
2022-01-17 09:25:56 +0100

    Document results of latest run

--- train_dataset/train_random_classifier.py
+++ train_dataset/train_random_classifier.py
@@ -32,8 +32,8 @@ structures_per_spg = 1
 #spgs = [129, 176] # 93.15%, pretty damn well!
 #spgs = [2, 15] # pretty much doesn't work at all (so far!), val_acc ~40%, after a full night: ~43%
 # after a full night with random volume factors: binary_accuracy: 0.7603 - val_loss: 0.8687 - val_binary_accuracy: 0.4749; still bad
-
 #spgs = [14,104,129,176] # after 100 epochs: 0.8503 val accuracy
+# all spgs (~200): loss: sparse_categorical_accuracy: 0.1248 - val_sparse_categorical_accuracy: 0.0713; it is a beginning
 
 # like in the Vecsei paper:
 start_angle, end_angle, N = 5, 90, 8501
