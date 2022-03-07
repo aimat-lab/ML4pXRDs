868a31f1c1c12c1c75799cfc6415ca10f3557431
Henrik Schopmans <h.schopmans@gmail.com>
2022-03-07 08:32:10 +0100

    Change log test to 2-15

--- train_dataset/train_random_classifier.py
+++ train_dataset/train_random_classifier.py
@@ -45,7 +45,7 @@ NO_epochs = 20  # TODO: Change back
 
 # structures_per_spg = 1 # for all spgs
 # structures_per_spg = 5
-structures_per_spg = 5  # for (2,15) tuple
+structures_per_spg = 10  # for (2,15) tuple
 NO_corn_sizes = 5
 # => 4*5*5=100 batch size (for 4 spgs)
 do_distance_checks = False
@@ -77,12 +77,12 @@ if local:
 
 # spgs = [14, 104] # works well, relatively high val_acc
 # spgs = [129, 176] # 93.15%, pretty damn well!
-# spgs = [
-#    2,
-#    15,
-# ]  # pretty much doesn't work at all (so far!), val_acc ~40%, after a full night: ~43%
+spgs = [
+    2,
+    15,
+]  # pretty much doesn't work at all (so far!), val_acc ~40%, after a full night: ~43%
 # after a full night with random volume factors: binary_accuracy: 0.7603 - val_loss: 0.8687 - val_binary_accuracy: 0.4749; still bad
-spgs = [14, 104, 129, 176]  # after 100 epochs: 0.8503 val accuracy
+# spgs = [14, 104, 129, 176]  # after 100 epochs: 0.8503 val accuracy
 # all spgs (~200): loss: sparse_categorical_accuracy: 0.1248 - val_sparse_categorical_accuracy: 0.0713; it is a beginning!
 
 # as Park:
