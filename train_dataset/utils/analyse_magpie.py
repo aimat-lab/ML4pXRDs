1869db030f1de21422ce242a1669a4d280f6ce27
Henrik Schopmans <h.schopmans@gmail.com>
2022-03-14 11:54:11 +0100

    Fix import problems with magpie

--- train_dataset/utils/analyse_magpie.py
+++ train_dataset/utils/analyse_magpie.py
@@ -1,5 +1,7 @@
-from magpie_python import StoichiometricAttributeGenerator
-from magpie_python.vassal.data import Cell
+from dataset_simulations.random_simulation_utils import generate_structures
+
+from magpie_python.data.materials import CrystalStructureEntry
+from magpie_python.vassal.data.Cell import Cell
 
 
 def plot_magpie_features(crystal_lists, label_per_list):
@@ -14,4 +16,12 @@ def plot_magpie_features(crystal_lists, label_per_list):
 
             cell = Cell()
 
-            cell.set_basis()
+            # cell.set_basis()
+
+
+if __name__ == "__main__":
+
+    test_0 = generate_structures(15, 10)
+    test_1 = generate_structures(15, 10)
+
+    plot_magpie_features([test_0, test_1], ["test_0", "test_1"])
