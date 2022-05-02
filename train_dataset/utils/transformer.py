a1b939a7b7774fa324ea37895842cb39dd53a809
Henrik Schopmans <h.schopmans@gmail.com>
2022-05-02 12:32:31 +0200

    Clean up transformer model

--- train_dataset/utils/transformer.py
+++ train_dataset/utils/transformer.py
@@ -3,6 +3,7 @@
 import tensorflow.keras as keras
 from tensorflow_addons.layers import MultiHeadAttention
 from tensorflow.keras import backend as K
+import numpy as np
 
 class Time2Vec(keras.layers.Layer):
     def __init__(self, kernel_size=1):
@@ -62,25 +63,52 @@ class AttentionBlock(keras.Model):
         return x
 
 class ModelTrunk(keras.Model):
-    def __init__(self, name='ModelTrunk', time2vec_dim=1, num_heads=2, head_size=128, ff_dim=None, num_layers=1, dropout=0, **kwargs):
+    def __init__(self, input_shape, name='ModelTrunk', time2vec_dim=1, num_heads=2, head_size=128, ff_dim=None, num_layers=1, dropout=0, **kwargs):
         super().__init__(name=name, **kwargs)
         self.time2vec = Time2Vec(kernel_size=time2vec_dim)
         if ff_dim is None:
             ff_dim = head_size
         self.dropout = dropout
         self.attention_layers = [AttentionBlock(num_heads=num_heads, head_size=head_size, ff_dim=ff_dim, dropout=dropout) for _ in range(num_layers)]
+        self.shape = input_shape
 
         
     def call(self, inputs):
-        time_embedding = keras.layers.TimeDistributed(self.time2vec)(inputs)
-        x = K.concatenate([inputs, time_embedding], -1)
+        time_embedding = keras.layers.TimeDistributed(self.time2vec, input_shape=self.shape)(inputs)
+        x = K.concatenate([inputs, time_embedding], -1) # 1+2=3
+
+        #print(x.shape)
+
         for attention_layer in self.attention_layers:
             x = attention_layer(x)
+
+            #print(x.shape)
 
         return K.reshape(x, (-1, x.shape[1] * x.shape[2])) # flat vector of features out
 
 def get_transformer():
 
-    model = ModelTrunk()
+    model = ModelTrunk((None,7,1), num_heads=2, head_size=128)
+
+    model.compile(
+        optimizer=keras.optimizers.Adam(),
+        loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        metrics=[
+            keras.metrics.SparseCategoricalAccuracy()
+        ],  # here from_logits is not needed, since argmax will be the same
+    )
 
     return model
+
+if __name__ == "__main__":
+
+    model = get_transformer()
+
+    print(model.predict(np.expand_dims(np.array([[1,5,2,4,5,2,5]], dtype=float), -1)))
+
+    # TODO: Remove the periodic component
+
+    # Why do we call MultiHeadAttention only with key, value?
+    #query = inputs[0]
+    #key = inputs[1]
+    #value = inputs[2] if len(inputs) > 2 else key
