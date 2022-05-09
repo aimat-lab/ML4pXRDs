29d551268719149ad1223d49f00497d34e2cc5ef
Henrik Schopmans <h.schopmans@gmail.com>
2022-05-09 13:49:28 +0200

    Fix bug in original model by Park et al.

--- train_dataset/models.py
+++ train_dataset/models.py
@@ -253,7 +253,11 @@ def build_model_park_huge_size(
 
 
 def build_model_park_original_spg(
-    hp=None, number_of_input_values=9018, number_of_output_labels=2, use_dropout=False
+    hp=None,
+    number_of_input_values=9018,
+    number_of_output_labels=2,
+    use_dropout=False,
+    lr=0.001,
 ):
     # From Park:
     # They actually train for 5000 epochs and batch size 1000 in the original paper
@@ -304,22 +308,10 @@ def build_model_park_original_spg(
     if use_dropout:
         model.add(keras.layers.Dropout(0.5))
 
-    model.add(
-        # keras.layers.Dense(
-        #    1 if (number_of_output_labels == 2) else number_of_output_labels
-        # )
-        keras.layers.Dense(number_of_output_labels)
-    )
+    model.add(keras.layers.Dense(number_of_output_labels))
 
-    optimizer = keras.optimizers.Adam(learning_rate=0.001)
+    optimizer = keras.optimizers.Adam(learning_rate=lr)
 
-    # if number_of_output_labels == 2:
-    #    model.compile(
-    #        optimizer=optimizer,
-    #        loss=keras.losses.BinaryCrossentropy(from_logits=True),
-    #        metrics=[BinaryAccuracy(from_logits=True)],
-    #    )
-    # else:
     model.compile(
         optimizer=optimizer,
         loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
