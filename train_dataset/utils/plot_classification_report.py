65ba890d58d52f4c7d85fecf72e5a52a1ad5c49e
Henrik Schopmans <h.schopmans@gmail.com>
2022-06-23 16:11:23 +0200

    Update plot_classification_report.py to get f1-score per crystal system

--- train_dataset/utils/plot_classification_report.py
+++ train_dataset/utils/plot_classification_report.py
@@ -1,4 +1,5 @@
 from concurrent.futures import process
+from xml.etree.ElementInclude import include
 from sklearn.metrics import classification_report
 from dataset_simulations.random_simulation_utils import load_dataset_info
 import pickle
@@ -9,7 +10,9 @@ import numpy as np
 
 if __name__ == "__main__":
 
-    if False:
+    include_NO_wyckoffs = False
+
+    if True:
 
         # report = classification_report(
         #    [1, 2, 3, 4, 5, 6, 10, 2, 5, 6, 3, 6, 7, 1, 2, 1, 3, 6, 4],
@@ -19,7 +22,7 @@ if __name__ == "__main__":
 
         # path = "/home/henrik/Dokumente/Masterarbeit/HEOs_MSc/train_dataset/classifier_spgs/runs_from_cluster/continued_tests/07-04-2022_14-55-52"  # 1-230
         # path = "/home/henrik/Dokumente/Masterarbeit/HEOs_MSc/train_dataset/classifier_spgs/runs_from_cluster/continued_tests/09-04-2022_12-27-30"  # 100-230
-        path = "/home/henrik/Dokumente/Masterarbeit/HEOs_MSc/train_dataset/classifier_spgs/runs_from_cluster/continued_tests/09-04-2022_22-56-44"  # 50-230
+        path = "/home/henrik/Dokumente/Masterarbeit/HEOs_MSc/train_dataset/classifier_spgs/runs_from_cluster/continued_tests/07-06-2022_09-43-41"  # 50-230
 
     else:
 
@@ -71,31 +74,32 @@ if __name__ == "__main__":
     metrics_match = f1_scores_match
     metric_name = "f1_scores"
 
-    (
-        probability_per_spg_per_element,
-        probability_per_spg_per_element_per_wyckoff,
-        NO_wyckoffs_prob_per_spg,
-        corrected_labels,
-        files_to_use_for_test_set,
-        represented_spgs,
-        NO_unique_elements_prob_per_spg,
-        NO_repetitions_prob_per_spg_per_element,
-        denseness_factors_density_per_spg,
-        kde_per_spg,
-        all_data_per_spg_tmp,
-    ) = load_dataset_info()
+    if include_NO_wyckoffs:
+        (
+            probability_per_spg_per_element,
+            probability_per_spg_per_element_per_wyckoff,
+            NO_wyckoffs_prob_per_spg,
+            corrected_labels,
+            files_to_use_for_test_set,
+            represented_spgs,
+            NO_unique_elements_prob_per_spg,
+            NO_repetitions_prob_per_spg_per_element,
+            denseness_factors_density_per_spg,
+            kde_per_spg,
+            all_data_per_spg_tmp,
+        ) = load_dataset_info()
 
-    average_NO_wyckoffs = []
+        average_NO_wyckoffs = []
 
-    for spg in spgs_match:
-        NO_wyckoffs_prob = NO_wyckoffs_prob_per_spg[spg]
+        for spg in spgs_match:
+            NO_wyckoffs_prob = NO_wyckoffs_prob_per_spg[spg]
 
-        average = 0
+            average = 0
 
-        for i, NO_wyckoff in enumerate(range(1, len(NO_wyckoffs_prob) + 1)):
-            average += NO_wyckoff * NO_wyckoffs_prob[i]
+            for i, NO_wyckoff in enumerate(range(1, len(NO_wyckoffs_prob) + 1)):
+                average += NO_wyckoff * NO_wyckoffs_prob[i]
 
-        average_NO_wyckoffs.append(average)
+            average_NO_wyckoffs.append(average)
 
     plt.figure()
     hd0 = plt.plot(spgs_match, metrics_match, label="Match")
@@ -105,12 +109,16 @@ if __name__ == "__main__":
         plt.axvline(x, color="r")
     plt.xlabel("spg")
     plt.ylabel(metric_name)
-    ax2 = plt.gca().twinx()
-    hd3 = ax2.plot(
-        spgs_match, average_NO_wyckoffs, label="Average NO_wyckoffs", color="r"
-    )
-    ax2.set_ylabel("average NO_wyckoffs")
-    plt.legend(handles=hd0 + hd1 + hd3)
+
+    if include_NO_wyckoffs:
+        ax2 = plt.gca().twinx()
+        hd3 = ax2.plot(
+            spgs_match, average_NO_wyckoffs, label="Average NO_wyckoffs", color="r"
+        )
+        ax2.set_ylabel("average NO_wyckoffs")
+        plt.legend(handles=hd0 + hd1 + hd3)
+    else:
+        plt.legend()
     plt.show()
 
     plt.figure()
@@ -124,10 +132,59 @@ if __name__ == "__main__":
         plt.axvline(x, color="r")
     plt.xlabel("spg")
     plt.ylabel("delta " + metric_name)
-    ax2 = plt.gca().twinx()
-    hd2 = ax2.plot(
-        spgs_match, average_NO_wyckoffs, label="Average NO_wyckoffs", color="r"
-    )
-    ax2.set_ylabel("average NO_wyckoffs")
-    plt.legend(handles=hd0 + hd2)
+
+    if include_NO_wyckoffs:
+        ax2 = plt.gca().twinx()
+        hd2 = ax2.plot(
+            spgs_match, average_NO_wyckoffs, label="Average NO_wyckoffs", color="r"
+        )
+        ax2.set_ylabel("average NO_wyckoffs")
+        plt.legend(handles=hd0 + hd2)
+    else:
+        plt.legend()
     plt.show()
+
+    metrics_per_crystal_system = {
+        "triclinic": 0,
+        "monoclinic": 0,
+        "orthorhombic": 0,
+        "tetragonal": 0,
+        "trigonal": 0,
+        "hexagonal": 0,
+        "cubic": 0,
+    }
+    total_per_crystal_system = {
+        "triclinic": 0,
+        "monoclinic": 0,
+        "orthorhombic": 0,
+        "tetragonal": 0,
+        "trigonal": 0,
+        "hexagonal": 0,
+        "cubic": 0,
+    }
+
+    for i, spg in enumerate(spgs_match):
+        if spg < 3:
+            crystal_system = "triclinic"
+        elif spg < 16:
+            crystal_system = "monoclinic"
+        elif spg < 75:
+            crystal_system = "orthorhombic"
+        elif spg < 143:
+            crystal_system = "tetragonal"
+        elif spg < 168:
+            crystal_system = "trigonal"
+        elif spg < 195:
+            crystal_system = "hexagonal"
+        else:
+            crystal_system = "cubic"
+
+        total_per_crystal_system[crystal_system] += 1
+        metrics_per_crystal_system[crystal_system] += metrics_match[i]
+
+    for key in metrics_per_crystal_system.keys():
+        metrics_per_crystal_system[key] /= total_per_crystal_system[key]
+
+    print(metrics_per_crystal_system)
+
+    # TODO: Maybe weight by number of samples
