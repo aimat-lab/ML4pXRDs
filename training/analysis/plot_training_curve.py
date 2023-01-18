15305b7cf39108ab2d7c071ab408698b3e872523
Henrik Schopmans <h.schopmans@gmail.com>
2023-01-18 11:41:49 +0100

    Update training curve script

--- training/analysis/plot_training_curve.py
+++ training/analysis/plot_training_curve.py
@@ -14,6 +14,7 @@ def plot_training_curve(
     linestyles,
     x_log=True,
     y_log=True,
+    do_fix_step_continuity=False,
 ):
     """Plot training curve from csv files (as downloaded from tensorboard).
 
@@ -25,6 +26,8 @@ def plot_training_curve(
         linestyles (list): Linestyle for each item of `files`.
         x_log (bool, optional): Use log scale for x axis. Defaults to False.
         y_log (bool, optional): Use log scale for y axis. Defaults to False.
+        do_fix_step_continuity (bool, optional): If two parts of a run that should be concatenated start at epoch 0,
+            this fixes the continuity of the epoch.
     """
 
     for i, file in enumerate(files):
@@ -37,7 +40,9 @@ def plot_training_curve(
             for subfile in file:
 
                 data = np.genfromtxt(subfile, skip_header=1, delimiter=",")[:, 1:3]
-                data[:, 0] += current_step
+
+                if do_fix_step_continuity:
+                    data[:, 0] += current_step
 
                 if all_data is None:
                     all_data = data
@@ -49,9 +54,11 @@ def plot_training_curve(
         else:
             all_data = np.genfromtxt(file, skip_header=1, delimiter=",")[:, 1:3]
 
+        x_data = all_data[:, 0]
+        y_data = all_data[:, 1]
         plt.plot(
-            all_data[:, 0],
-            all_data[:, 1],
+            x_data,
+            y_data,
             label=labels[i],
             color=colors[i],
             linestyle=linestyles[i],
@@ -129,14 +136,69 @@ if __name__ == "__main__":
 
     plot_training_curve(
         [
-            "training_curves/resnet_10_random.csv",
+            "training_curves/resnet_10_training.csv",
             "training_curves/resnet_10_match.csv",
             "training_curves/resnet_10_top5.csv",
         ],
-        ["ResNet-10 training", "ResNet-10 ICSD", "ResNet-10 ICSD top-5"],
+        [
+            "ResNet-10 training",
+            "ResNet-10 ICSD",
+            "ResNet-10 ICSD top-5",
+        ],
         ["r", "g", "b"],
         ["solid", "solid", "solid"],
     )
 
+    plot_training_curve(
+        [
+            [
+                "training_curves/resnet_50_training_0.csv",
+                "training_curves/resnet_50_training_1.csv",
+            ],
+            [
+                "training_curves/resnet_50_match_0.csv",
+                "training_curves/resnet_50_match_1.csv",
+            ],
+            [
+                "training_curves/resnet_50_top5_0.csv",
+                "training_curves/resnet_50_top5_1.csv",
+            ],
+        ],
+        [
+            "ResNet-50 training",
+            "ResNet-50 ICSD",
+            "ResNet-50 ICSD top-5",
+        ],
+        ["r", "g", "b"],
+        [(0, (1, 5)), (0, (1, 5)), (0, (1, 5))],
+    )
+
+    plot_training_curve(
+        [
+            [
+                "training_curves/resnet_101_training_0.csv",
+                "training_curves/resnet_101_training_1.csv",
+            ],
+            [
+                "training_curves/resnet_101_match_0.csv",
+                "training_curves/resnet_101_match_1.csv",
+            ],
+            [
+                "training_curves/resnet_101_top5_0.csv",
+                "training_curves/resnet_101_top5_1.csv",
+            ],
+        ],
+        [
+            "ResNet-101 training",
+            "ResNet-101 ICSD",
+            "ResNet-101 ICSD top-5",
+        ],
+        ["r", "g", "b"],
+        [(0, (1, 10)), (0, (1, 10)), (0, (1, 10))],
+    )
+
     plt.savefig("training_curve_main.pdf")
     plt.show()
+
+    # TODO: Switch color and line style
+    # TODO: Fix the inconsistent spacing between datapoints somehow
